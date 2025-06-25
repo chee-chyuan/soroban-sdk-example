c5a95ed5d1bedbfce21527bf596ebc80a82da76f
Chee Chyuan Ang <cheechyuanang@Chees-MacBook-Pro-2.local>
2025-06-25 20:55:38 +0800

    use feature alloc

--- groth16_bn254_verifier/src/lib.rs
+++ groth16_bn254_verifier/src/lib.rs
@@ -14,14 +14,6 @@ mod fields;
 mod groth16;
 mod verifier;
 
-// extern crate alloc;
-
-extern crate wee_alloc;
-
-// Use `wee_alloc` as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[contract]
 pub struct Groth16BN254Verifier;
 
